c3a01b163e9f1d35e5b60bf32a31956212a4ab67
Oguzhan Calikkasap <oguzhanca94@gmail.com>
2018-11-27 22:51:28 +0300

    k-means bug fixes

--- Homeworks/Hw2/kmeans.py
+++ Homeworks/Hw2/kmeans.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: oguzhan
-"""
+Name: Oguzhan Calikkasap
+ID: 21801131
 
+"""
+import numpy as np
 import scipy.io
 import imageio
-import numpy as np
 import pylab
+import time
 import matplotlib.pyplot as plt
 from PIL import Image
-import time
 
 
 class Kmeans:
@@ -27,7 +28,7 @@ class Kmeans:
         return centroid_locs
 
 
-    def calc_Centr_Error(self, mse, k, labels):
+    def update_centroids(self, mse, k, labels):
         current_centroid = np.zeros((k, 3))
         error = 0
         for i in range(k):
@@ -52,7 +53,7 @@ class Kmeans:
         for i in range(k):
             dist[:, i] = np.linalg.norm(im2 - centroids[i], axis=1)
         labels = np.argmin(dist, axis=1)
-        centroids, mse = self.calc_Centr_Error(mse, k, labels)
+        centroids, mse = self.update_centroids(mse, k, labels)
 
         while not (labels == np.argmin(dist, axis=1)).all():
 
@@ -61,51 +62,45 @@ class Kmeans:
                 dist[:, i] = np.linalg.norm(im2 - centroids[i], axis=1)
                 labels = np.argmin(dist, axis=1)
 
-            centroids, mse = self.calc_Centr_Error(mse, k, labels)
+            centroids, mse = self.update_centroids(mse, k, labels)
             iteration += 1
 
         return labels, centroids, mse
 
 
     def fit(self, im, im2):
-        clusteredImages = np.zeros((self.num_of_K, im.shape[0], im.shape[1], 3), dtype=np.uint8)
+        clustered = np.zeros((self.num_of_K, im.shape[0], im.shape[1], 3), dtype=np.uint8)
         err = []
-
 
         for k in range(self.num_of_K):
             print("Computing for K =", self.K[k])
 
             labels, centroids, mse = self.predict(self.K[k], im2)
-
+            err.append(mse)
             labels = labels.reshape((im.shape[0], im.shape[1])).T
-            clusteredImage = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.uint8)
+            clustered_im = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.uint8)
 
             for i in range(im.shape[0]):
                 for j in range(im.shape[1]):
-                    clusteredImage[i, j, :] = centroids[labels[j, i], :].astype(np.uint8)
+                    clustered_im[i, j, :] = centroids[labels[j, i], :].astype(np.uint8)
 
-            clusteredImages[k, :, :, :] = clusteredImage
-            err.append(mse)
+            clustered[k, :, :, :] = clustered_im
 
             print("Mean Squared Error = ", np.round(mse, decimals=2))
             print("Centroids =\n", np.around(centroids).astype(int))
             print("------------------------")
 
-            imageio.imwrite("./q1_k" + str(self.K[k]) + ".png", clusteredImage)
-
         for i in range(self.num_of_K):
-            pylab.figure()
-            pylab.imshow(clusteredImages[i, :, :, :])
-            pylab.title("K: " + str(self.K[i]))
+            plt.figure()
+            pylab.imshow(clustered[i, :, :, :])
+            pylab.title("K: {}".format(self.K[i]))
 
             self.all_error.append(np.round(err[i], decimals=2))
 
         plt.figure()
         plt.plot(self.K, self.all_error)
-        plt.title("Error Graph")
         plt.xlabel("Number of K")
         plt.ylabel("Mean squared error")
-        plt.savefig("./Error_graph.png")
         plt.show()
 
 #%% Main
@@ -117,13 +112,13 @@ im = np.array(im)
 im2 = im.reshape(-1, 3)
 print('\nimage read.\n')
 
-K = (2, 4, 8)#, 16)#, 32, 64, 128)
+K = (2, 4, 8, 16, 32)#, 64, 128, 256)
 
 start_time = time.time()
 
 k_means = Kmeans(K)
 k_means.fit(im, im2)
-print("Error List: ", k_means.all_error)
+print("MSE List: ", k_means.all_error)
 print("\nK-values computed: ", K)
 print("Time passed: ", np.round((time.time() - start_time), decimals=2), "seconds.")
-print("Program finished.")
+print("Process finished.")
